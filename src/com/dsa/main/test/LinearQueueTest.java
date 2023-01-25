e4dbec5e2601e841d9d2cd9418e74f45e9d85a50
Arpit Parasana <au.parasana@gmail.com>
2023-01-25 10:23:34 -0500

    add linear queue main test

---
+++ src/com/dsa/main/test/LinearQueueTest.java
@@ -0,0 +1,41 @@
+package com.dsa.main.test;
+
+import com.dsa.structures.LinearQueueInt;
+
+public class LinearQueueTest {
+
+	public static void main(String[] args) {
+		LinearQueueInt queue = new LinearQueueInt(5);
+		
+		System.out.println("Queue elements: " + queue.display());
+		
+		queue.push(10);
+		queue.push(11);
+		System.out.println("Queue elements: " + queue.display());
+		
+		System.out.println("Removed - " + queue.pop());
+		System.out.println("Queue elements: " + queue.display());
+		
+		System.out.println("Removed - " + queue.pop());
+		System.out.println(queue.display());
+		
+		queue.push(1);
+		queue.push(2);
+		System.out.println("Queue elements: " + queue.display());
+		
+		queue.push(45);
+		queue.push(67);
+		queue.push(87);
+		System.out.println("Queue elements: " + queue.display());
+
+		System.out.println("Removed - " + queue.pop());
+		System.out.println("Removed - " + queue.pop());
+		System.out.println("Removed - " + queue.pop());
+		System.out.println("Removed - " + queue.pop());
+		System.out.println("Queue elements: " + queue.display());
+		
+		// queue.push(87); -- this will cause error as Queue needs to be cleared before use
+
+		
+	}
+}
